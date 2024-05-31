08d0d2cdd94c8877792baf749e06ce97c6410f9d
Ali Karagöz <80913896+aliblackeye@users.noreply.github.com>
2024-05-31 23:35:23 +0300

    new: data usage per network

--- app/(www)/dashboard/_partials/data-usage-per-network.tsx
+++ app/(www)/dashboard/_partials/data-usage-per-network.tsx
@@ -1,16 +1,59 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+"use client";
 
+import { Line } from "react-chartjs-2";
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+	Point,
+	ChartData,
+} from "chart.js";
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend
+);
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+const data: ChartData<"line", (number | Point | null)[], unknown> = {
+	labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+	datasets: [
+		{
+			label: "My First Dataset",
+			data: [1, 3, 5, 10],
+			fill: false,
+			borderColor: "#1C1C1C",
+			borderWidth: 3,
+			tension: 0.4,
+			pointBorderWidth: 0,
+		},
+	],
+};
 export const DataUsagePerNetwork = () => {
 	return (
 		<section className="mb-[39px] w-full">
-			<Card className="h-[443px] p-6">
+			<Card className="h-fit p-6">
 				<CardHeader className="!p-0">
 					<CardTitle className="text-xl leading-5 font-extrabold mb-3">
 						Data usage per network
 					</CardTitle>
 				</CardHeader>
 				<CardContent>
-					<div className="h-[359px]"></div>
+					<div className="h-max w-full">
+						<Line
+							className="w-full"
+							options={{}}
+							data={data}
+						/>
+					</div>
 				</CardContent>
 			</Card>
 		</section>
