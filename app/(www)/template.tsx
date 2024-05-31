a5f463d9b2458729b6cc809b88a5f49b7d8285a8
Ali Karagöz <80913896+aliblackeye@users.noreply.github.com>
2024-05-31 03:06:02 +0300

    new: header

--- app/(www)/template.tsx
+++ app/(www)/template.tsx
@@ -12,9 +12,9 @@ export default function WwwTemplate({
 		return (
 			<div className="flex min-h-screen w-full flex-col bg-[#f7fafc]">
 				<Sidebar />
-				<div className="flex flex-col">
-					{/* <Header /> */}
-					{children}
+				<div className="flex flex-col mx-auto lg:max-w-[892px] w-full">
+					<Header />
+					<main>{children}</main>
 				</div>
 			</div>
 		);
---
+++ layouts/header/_partials/special-offer.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { X } from "lucide-react";
+import Link from "next/link";
+import { useCallback } from "react";
+
+export default function SpecialOffer() {
+	const handleClose = useCallback(() => {
+		// Todo: Close
+	}, []);
+	return (
+		<div className="special-offer bg-[#78B6FF]/30 p-4 pl-[22px] pr-[24px] flex justify-between mt-[21px] rounded-[5px] mb-9">
+			<p className="text-sm font-medium">
+				Special Offer! Get Complete Free Proxy 10 MB Proxy, without credit card.{" "}
+				<Link
+					href={"#"}
+					className="underline">
+					Start Free Trial
+				</Link>
+			</p>
+			<X
+				className="w-5 h-5 text-[#7E868C] cursor-pointer"
+				onClick={handleClose}
+			/>
+		</div>
+	);
+}
